50e64a6a44cc1b8b8dad40599919f74c819423fd
i5m <i5ivem@gmail.com>
2010-03-07 23:55:06 +0000

    Fix alignment of edit_lines
    As per this Stackoverflow question:
    http://stackoverflow.com/questions/584256/alignment-of-edit-line/653142#653142
    Have verified fix, but not investigated any consequences.
    
    "
    Shoes.app do
        edit_line("Something")
        edit_line("Something Else")
    end
    
    the second edit_line control seems to be placed 4 pixels lower than the first one
    "

--- shoes/ruby.c
+++ shoes/ruby.c
@@ -3435,8 +3435,8 @@ shoes_edit_line_draw(VALUE self, VALUE c, VALUE actual)
   SETUP_CONTROL(0, 0, FALSE);
 
 #ifdef SHOES_QUARTZ
-  place.x += 4; place.ix += 4;
-  place.y += 4; place.iy += 4;
+  /*place.x += 4; place.ix += 4;*/
+  /*place.y += 4; place.iy += 4;*/
   place.h += 4; place.ih += 4;
   place.w += 4; place.iw += 4;
 #endif
